388219e3c33c6c3631ab302b54b22bed78b74022
ADUL S <122665910+Adulsatl@users.noreply.github.com>
2024-09-09 16:43:08 +0530

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -10,18 +10,16 @@ def determine_winner(user_choice, computer_choice):
          (user_choice == 'paper' and computer_choice == 'rock'):
         return "You win!"
     else:
-        return "Sorry!!You lose!:("
+        return "Sorry!!!You lose! :("
 def play_game():
     print("Welcome to Rock Paper Scissor Game!")
     user_score = 0
     computer_score = 0
-
     while True:
         user_choice = input("Enter rock,paper,or scissor: ").lower()
         if user_choice not in ['rock', 'paper', 'scissor']:
             print("Invalid choice,Try again.")
             continue
-
         computer_choice = get_computer_choice()
         print(f"Computer chose: {computer_choice}")
         result = determine_winner(user_choice, computer_choice)
@@ -30,10 +28,9 @@ def play_game():
             user_score += 1
         elif result == "You lose!":
             computer_score += 1
-
         print(f"Score -> You: {user_score}, Computer: {computer_score}")
-        play_again = input("Do you want to play again? (yes/no): ").lower()
+        play_again = input("Do you want to play again? (yes/no)").lower()
         if play_again != 'yes':
             break
-    print(f"Thank you for playing! Final Score -> You: {user_score}, Computer: {computer_score}")
+    print(f"Thank you for playing RPS! Final Score -> You: {user_score},Computer:{computer_score}")
 play_game()
